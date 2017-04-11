a54cdc5ae5073e24248cfb962dd907ca2d556d13
talonbragg <pvpgamecrash@gmail.com>
2017-04-11 12:36:02 -0700

    Yay

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.static('assets'));
 //Main Index file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+  model.mainObjSchema => (mainProfileObj);
 });
 
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", (req, res) => {
